540026d340e25cf369b3272301346b08a3986f17
thuyatun <tunthura97@gmail.com>
2018-06-07 09:23:21 +0630

    my first python ex

--- ex9.py
+++ ex9.py
@@ -1,5 +1,7 @@
 
  # Here's some new strange stuff, remember type it exactly.
-2 3 days = "Mon Tue Wed Thu Fri Sat Sun" 4 months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
-5 6 print("Here are the days: ", days) 7 print("Here are the months: ", months)
-8 9 print(""" 10 There's something going on here. 11 With the three double-quotes. 12 We'll be able to type as much as we like. 13 Even 4 lines if we want, or 5, or 6. 14 """)
+ days = "Mon Tue Wed Thu Fri Sat Sun" 
+ months = "Jan \n Feb \n Mar \n Apr \n May \n Jun\n Jul \n Aug"
+ print("Here are the days: ", days) 
+ print("Here are the months: ", months)
+print(""" There's something going on here.  With the three double-quotes. we'll be able to type as much as we like. 13 Even 4 lines if we want, or 5, or 6. 14 """)
