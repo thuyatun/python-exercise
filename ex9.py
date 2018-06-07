58daf9b465eee2908c3e9dcde50b9b06ad893e24
KaungHtettAung <kaunghtett1997@gmail.com>
2018-06-07 22:29:35 +0630

    change data: :

--- ex9.py
+++ ex9.py
@@ -1,7 +1,7 @@
 
  # Here's some new strange stuff, remember type it exactly.
- days ="Mon Tue Wed Thu Fri Sat Sun" 
- months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+ days =Mon Tue Wed Thu Fri Sat Sun 
+ months = iJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug
  print(f"Here are the days: {days}") 
  print(f"Here are the months: {months}")
 print(""" There's something going on here.  With the three double-quotes. we'll be able to type as much as we like. 13 Even 4 lines if we want, or 5, or 6. 14 """)
