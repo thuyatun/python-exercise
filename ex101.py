227609e7ccee65197293536f69f27802bf1573af
MyoAung <thuyatun>
2018-06-26 14:16:56 +0630

    change name

--- ex101.py
+++ ex101.py
@@ -1,12 +1,12 @@
-tabby_cat = "\tI'm tabbed in." 
-persian_cat = "I'm split\non a line." 
-backslash_cat = "I'm \\ a \\ cat."
-fat_cat = """ I'll do a list: 
-    \t* Cat food 
+tabby_dog = "\tI'm tabbed in." 
+persian_dog = "I'm split\non a line." 
+backslash_dog = "I'm \\ a \\ dog."
+fat_dog = """ I'll do a list: 
+    \t* Dog food 
     \t* Fishies 
-    \t* Catnip
+    \t* Dognip
     \t* Grass """
-print(tabby_cat)
-print(persian_cat)
-print(backslash_cat) 
-print(fat_cat)
+print(tabby_dog)
+print(persian_dog)
+print(backslash_dog) 
+print(fat_dog)
