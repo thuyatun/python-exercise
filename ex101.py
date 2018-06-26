01154b41667a0fcd4b05c843f4b90c7843fe99eb
thuyatun <tunthura97@gmail.com>
2018-06-26 15:10:24 +0630

    Update ex101.py

--- ex101.py
+++ ex101.py
@@ -1,12 +1,12 @@
-tabby_rat = "\tI'm tabbed in." 
-persian_rat = "I'm split\non a line." 
-backslash_rat = "I'm \\ a \\ rat."
-fat_rat = """ I'll do a list: 
-    \t* rat food 
+tabby_pet = "\tI'm tabbed in." 
+persian_pet = "I'm split\non a line." 
+backslash_pet = "I'm \\ a \\ pet."
+fat_pet = """ I'll do a list: 
+    \t* pet food 
     \t* Fishies 
-    \t* ratnip
+    \t* petnip
     \t* Grass """
-print(tabby_rat)
-print(persian_rat)
-print(backslash_rat) 
-print(fat_rat)
+print(tabby_pet)
+print(persian_pet)
+print(backslash_pet) 
+print(fat_pet)
